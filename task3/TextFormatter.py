60ab3fc4dee39742970341048e4dcb107b592b36
Emilio Domínguez <emilio.dominguezs@um.es>
2019-12-10 19:07:05 +0100

    added line length control to SpanishFormatter

--- task3/TextFormatter.py
+++ task3/TextFormatter.py
@@ -143,6 +143,8 @@ class SpanishFormatter(object):
                     print('', file=file)
                     print(token[0], end='', file=file)
                     self.currentSpace = self.len-len(token[0])
+                if self.currentSpace < 0:
+                    print(f'[{cls.__name__}] Exceptionally long word led to a bigger line.', file=sys.stderr)
             else:
                 # The first word goes without preceeding space
                 print(token[0], end='', file=file)
